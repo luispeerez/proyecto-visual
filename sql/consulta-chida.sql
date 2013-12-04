6b304e79feb58201e7453524464d4e02bd05cbaa
luis <luis19prz@gmail.com>
2013-12-03 19:52:38 -0600

    archivo de consultas

--- sql/consulta-chida.sql
+++ sql/consulta-chida.sql
@@ -3,7 +3,11 @@
 
 
 #El que usamos en el form
-SELECT pedido.idorden,tablota.nombre,tablota.precio,tablota.idalimento  FROM pedido  LEFT JOIN (SELECT *FROM alimento AS alimentodia WHERE idalimento IN (SELECT idalimento FROM pedido AS pedidos WHERE idorden IN (SELECT idorden FROM orden AS ordenes WHERE fecha LIKE '2013-12-02%')))  AS tablota USING(idalimento);
+SELECT pedido.idorden,tablota.nombre,tablota.precio,tablota.idalimento  FROM pedido  LEFT JOIN(
+	SELECT *FROM alimento AS alimentodia WHERE idalimento IN(
+		SELECT idalimento FROM pedido AS pedidos WHERE idorden IN(
+			SELECT idorden FROM orden AS ordenes WHERE fecha LIKE '2013-12-03%'))) AS tablota
+	USING(idalimento);
 	
 
 
@@ -30,12 +34,38 @@ SELECT  *FROM pedido LEFT JOIN((
 	)USING(idalimento)
 ;
 
-mysql> SELECT tablota.nombre,tablota.precio, count(*)  FROM pedido  LEFT JOIN (SELECT *FROM alimento AS alimentodia WHERE idalimento IN (SELECT idalimento FROM pedido AS pedidos WHERE idorden IN (SELECT idorden FROM orden AS ordenes WHERE fecha LIKE '2013-12-02%')))  AS tablota USING(idalimento) GROUP BY nombre;
+
+SELECT COUNT(*) FROM pedido WHERE idalimento =
+				(SELECT idalimento FROM pedido AS pedidos WHERE idorden IN(
+					SELECT idorden FROM orden AS ordenes WHERE fecha LIKE '2013-12-02%') )
+;
 
 
 
 
+#El definitivo
+ SELECT tablota.nombre,tablota.precio, count(*)  FROM pedido  LEFT JOIN 
+(SELECT *FROM alimento AS alimentodia WHERE idalimento IN 
+(SELECT idalimento FROM pedido AS pedidos WHERE idorden IN 
+(SELECT idorden FROM orden AS ordenes WHERE fecha LIKE '2013-12-02%')))  
+AS tablota USING(idalimento) GROUP BY nombre;
 
 
+#El de cobro
+SELECT pedido.idorden,tablota.nombre,tablota.precio,tablota.idalimento  FROM pedido  LEFT JOIN(
+	SELECT *FROM alimento AS alimentodia WHERE idalimento IN(
+		SELECT idalimento FROM pedido AS pedidos WHERE idorden IN(
+			SELECT idorden FROM orden AS ordenes WHERE idorden=1))) AS tablota
+	USING(idalimento);
+	
+	
+#Para resetear mesas
+SELECT *FROM mesa WHERE idmesa IN(
+	SELECT idmesa FROM orden WHERE idorden=1
+);
 
+UPDATE mesa SET estatus='Disponible',idcliente=null  WHERE idmesa IN(
+	SELECT idmesa FROM orden WHERE idorden=1
+);
 
+UPDATE orden SET estatus='PAGADA' WHERE idorden = 1;
